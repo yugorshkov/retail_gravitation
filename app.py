38319e5a2b91c0bc9b8deaefc4ea3eb48aed74ea
Yuriy <gyuv28@gmail.com>
2023-10-25 15:42:54 +0300

    Реализован основной функционал обработки данных

--- app.py
+++ app.py
@@ -1,19 +1,54 @@
+import json
+
 import folium
+import geopandas
 import streamlit as st
 from streamlit_folium import st_folium
 
-st.write('Retail Gravitation Test App')
+st.set_page_config(page_title="Retail Gravitation", page_icon="🛒")
+st.title("🏪 Retail Gravitation Test App")
 
-m = folium.Map(location=[45.035470, 38.975313], zoom_start=13)
+with open("data/cities.json", encoding="utf-8") as f:
+    cities = json.load(f)
+city = st.selectbox("Выберите город:", cities)
+area = st.number_input(
+    "Введите площадь магазина", min_value=25, max_value=5000, value=100
+)
+
+osm_id = cities[city]['osm_id']
+url = f"https://polygons.openstreetmap.fr/get_geojson.py?id={osm_id}&params=0"
+boundary = geopandas.read_file(url)
+CENTER_START = [boundary.centroid.y, boundary.centroid.x]
+
+m = folium.Map(location=CENTER_START, zoom_start=10)
+folium.TileLayer("cartodb positron", show=False).add_to(m)
+folium.GeoJson(
+    boundary,
+    name="границы города",
+    style_function=lambda feature: {
+        "color": "black",  # цвет границ объекта
+        "weight": 2,  # толщина линии
+        # "dashArray": "5, 5", # тип линии пунктир
+        "fillColor": "green",  # цвет заливки объекта
+        "fill_opacity": 0.1,  # прозрачность
+        "fill": True,  # будет ли заливка
+    },
+).add_to(m)
+folium.LayerControl().add_to(m)
 m.add_child(folium.ClickForMarker())
 st_data = st_folium(m, width=725)
+
 st_data
 
 
-lat, lng = st_data["last_clicked"]['lat'], st_data["last_clicked"]['lat']
-if 'markers' not in st.session_state:
-    st.session_state['markers'] = []
+lat, lng = st_data["last_clicked"]["lat"], st_data["last_clicked"]["lng"]
+if "markers" not in st.session_state:
+    st.session_state["markers"] = {}
 
-st.session_state['markers'].append([lat, lng])
+if "store_number" not in st.session_state:
+    st.session_state["store_number"] = 1
+store_name = f"Новый магазин {st.session_state['store_number']}"
+if f"{lng} {lat}" not in st.session_state["markers"]:
+    st.session_state["markers"][f"{lng} {lat}"] = (store_name, area)
+    st.session_state["store_number"] += 1
 st.write(st.session_state.markers)
-
--- src/main.py
+++ src/main.py
@@ -1,37 +1,35 @@
 import json
 import os
 
+import constants as c
 import geopandas
 import numpy as np
 import pyrosm
-from prefect import flow, task
-
 from crs import Albers_Equal_Area_Russia
-
-SA_STORE_AREA_LIMIT = 300  # 300-500
-SUPERMARKET_AREA_PART = 0.3  # 0.3-0.5
-CONVENIENCE_AREA_PART = 0.15  # 0.15-0.3
-WALKING_DISTANCE = 400  # 400-800
+from prefect import flow, task
+from supply import add_user_shops, func, measure_market_share
 
 
 @task(retries=3, retry_delay_seconds=5)
 def get_osm_data(city: dict, local_prefix: str = "data") -> None:
+    """Загружаем данные OpenStreetMap, обрезаем дамп по границе указанного города
+    и оставляем только те объекты OSM, которые нас интересуют."""
     os.system(
         f"src/osm_get_data.sh {city['region']} {local_prefix} {city['name']} {city['osm_id']}"
     )
 
 
-@task
-def process_apartment_buildings_data(path: str) -> geopandas.GeoDataFrame:
-    gdf = geopandas.read_file(path)
-    attrs = ["RMC", "RMC_LIVE", "INHAB", "geometry"]
-    gdf = gdf[attrs]
+@task(log_prints=True)
+def process_apartment_buildings_data(fp: str) -> geopandas.GeoDataFrame:
+    """Обрабатываем информацию по жилым многоквартирным домам для получения
+    ориентировочной численности жителей."""
+    gdf = geopandas.read_file(fp)
+    gdf = gdf[["HOUSE_ID", "RMC", "RMC_LIVE", "INHAB", "geometry"]]
 
     gdf = gdf.replace({"": np.nan})
     for col in ["RMC", "RMC_LIVE"]:
         gdf[col] = gdf[col].apply(lambda x: str(x).replace(" ", ""))
-    col_types = {attr: "float" for attr in attrs[:-1]}
-    gdf = gdf.astype(col_types)
+    gdf = gdf.astype({"RMC": float, "RMC_LIVE": float, "INHAB": float})
 
     mask = ((gdf["RMC_LIVE"] > 1000) & (gdf["RMC_LIVE"] > gdf["RMC"])) | gdf[
         "RMC_LIVE"
@@ -39,20 +37,38 @@ def process_apartment_buildings_data(path: str) -> geopandas.GeoDataFrame:
     gdf["RMC_LIVE"] = np.where(mask, gdf["RMC"], gdf["RMC_LIVE"])
 
     avg_residents_in_apartment = (gdf.INHAB / gdf.RMC_LIVE).mean().round(2)
+    print(f"Среднее кол-во жителей в одной квартире - {avg_residents_in_apartment}")
     gdf["inhab_calc"] = gdf["RMC_LIVE"] * avg_residents_in_apartment
     residents_in_house = gdf["inhab_calc"].mean()
     gdf["inhab_calc"] = gdf["inhab_calc"].fillna(residents_in_house)
-    gdf["inhab_calc"] = gdf[["INHAB", "inhab_calc"]].max(axis=1)
-    gdf["inhab_calc"] = gdf["inhab_calc"].astype(int)
-    gdf = gdf[["inhab_calc", "geometry"]]
+    gdf["INHAB"] = gdf[["INHAB", "inhab_calc"]].max(axis=1)
+    gdf["INHAB"] = gdf["INHAB"].astype(int)
+    gdf = gdf[["HOUSE_ID", "INHAB", "geometry"]]
+
+    gdf.to_crs(Albers_Equal_Area_Russia, inplace=True)
+    gdf["wlk_dist_zone"] = gdf.buffer(c.WALKING_DISTANCE)
+    gdf = gdf.set_geometry("wlk_dist_zone")
+    gdf.rename(columns={"geometry": "coords"}, inplace=True)
     return gdf
 
 
 @task
-def calculate_stores_area(fp) -> geopandas.GeoDataFrame:
+def calculate_stores_area(fp: str) -> geopandas.GeoDataFrame:
+    """Вычисляем площадь каждого магазина города, как часть площади здания в котором
+    он располагается. Логика определения площади следующая:
+    1. Если в данных OSM геометрия магазина представлена полигоном, считаем, что
+        магазин занимает всю влощадь полигона.
+    2. Для магазинов, которые представлены точкой, находим геометрию здания, которая
+        включает в себя эту точку. Вычисляем площадь здания.
+    3. Если площадь здания небольшая (регулируется параметром SUPERMARKET_AREA_PART),
+        принимаем площадь магазина равной площади здания.
+    4. Для оставшихся магазинов принимаем площадь равной площади здания с поправкой на
+        коэффициент доли площади магазина в здании:
+        - SUPERMARKET_AREA_PART для супермаркетов;
+        - CONVENIENCE_AREA_PART для небольших магазинов у дома."""
     osm = pyrosm.OSM(fp)
     buildings = osm.get_buildings()
-    buildings = buildings[["id", "building", "geometry"]]
+    buildings = buildings[["id", "geometry"]]
     shops = osm.get_pois(custom_filter={"shop": ["supermarket", "convenience"]})
     shops = shops[["id", "shop", "name", "geometry"]]
     buildings.to_crs(Albers_Equal_Area_Russia, inplace=True)
@@ -60,50 +76,46 @@ def calculate_stores_area(fp) -> geopandas.GeoDataFrame:
 
     shops["store_area"] = shops.area
     store_areas = buildings.sjoin(shops)
+    store_areas.drop("index_right", axis=1, inplace=True)
     store_type_coef = dict(
-        supermarket=SUPERMARKET_AREA_PART, convenience=CONVENIENCE_AREA_PART
+        supermarket=c.SUPERMARKET_AREA_PART, convenience=c.CONVENIENCE_AREA_PART
     )
     cond1 = np.where(
-        store_areas.area <= SA_STORE_AREA_LIMIT,
+        store_areas.area <= c.SA_STORE_AREA_LIMIT,
         store_areas.area,
         store_areas["shop"].map(store_type_coef) * store_areas.area,
     )
     cond2 = np.where(store_areas["store_area"] != 0, store_areas["store_area"], cond1)
     store_areas["store_area"] = cond2.astype("int")
+    store_areas.rename(
+        columns={"id_left": "building_id", "id_right": "shop_id"}, inplace=True
+    )
     return store_areas
 
 
-@task
-def func(residents, shops):
-    residents.to_crs(Albers_Equal_Area_Russia, inplace=True)
-    residents["wlk_dist_zone"] = residents.buffer(WALKING_DISTANCE)
-
-    gdf = shops.drop(columns=["index_right"]).sjoin(
-        residents.set_geometry("wlk_dist_zone").rename(
-            columns={"geometry": "house_geo"}
-        )
-    )
-    gdf["dist"] = gdf.distance(gdf["house_geo"])
-    gdf["attract"] = gdf["store_area"] / gdf["dist"] ** 2
-    gdf = gdf.assign(totattract=gdf.groupby("index_right")["attract"].transform("sum"))
-    gdf["marketshare"] = gdf["attract"] / gdf["totattract"]
-    return gdf
-
-
-@flow(name="Retail Gravitation")
+@flow(name="Retail Gravitation", log_prints=True)
 def main():
+    """"""
     with open("data/cities.json", encoding="utf-8") as f:
         cities = json.load(f)
 
-    get_osm_data(cities["krasnodar"])
-    gdf1 = process_apartment_buildings_data(
+    get_osm_data(cities["Краснодар"])
+    rsdnts = process_apartment_buildings_data(
         "data/myhouse_RU-CITY-016_points_matched.geojson"
     )
-    gdf2 = calculate_stores_area(
-        "data/krasnodar/krasnodar-shops-buildings.osm.pbf", Albers_Equal_Area_Russia
+    city_shops = calculate_stores_area(
+        "data/krasnodar/krasnodar-shops-buildings.osm.pbf"
     )
-    print(func(gdf1, gdf2, Albers_Equal_Area_Russia))
+
+    user_input = {
+        "38.98086547851563 45.2352504759637": ["Новый магазин 1", 100],
+        "39.01481151580811 45.07055046636354": ["Новый магазин 2", 50],
+        "39.00846004486085 45.06585248027519": ["Новый магазин 3", 350],
+    }
+    stores = add_user_shops(city_shops, user_input)
+    result = measure_market_share(rsdnts, stores)
+    print(func(result))
 
 
 if __name__ == "__main__":
-    main()
+    main.serve(name="test-dep")
